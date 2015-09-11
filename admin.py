6aac603b8e74c8bb4a29116b47c0f0145b3b79ac
PrinceCuberdon <princecuberdon@bandcochon.fr>
2015-09-11 21:28:26 +0200

    Backup start

--- admin.py
+++ admin.py
@@ -15,41 +15,48 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from django.contrib import admin
-from django import forms
 from django.forms.models import BaseInlineFormSet
 
-from libs.poll.models import *
+from libs.poll.models import Poll, Question
 
-class PollAdminForm(forms.ModelForm):
-    class Meta:
-        model = Poll
-        
-    def clean_name(self):
-        """ Check if there is overlaped polls """
-        
-        return self.cleaned_data
-    
+#
+# class PollAdminForm(forms.ModelForm):
+#
+#     class Meta:
+#         model = Poll
+#
+#     def clean_name(self):
+#         """ Check if there is overlaped polls """
+#
+#         return self.cleaned_data
+
+
 class RequiredInlineFormSet(BaseInlineFormSet):
     """ Found on Stackoverflow """
+
     def _construct_form(self, i, **kwargs):
         form = super(RequiredInlineFormSet, self)._construct_form(i, **kwargs)
         form.empty_permitted = False
         return form
-    
+
+
 class QuestionInline(admin.StackedInline):
     model = Question
     extra = 1
     formset = RequiredInlineFormSet
 
+
 class PollAdmin(admin.ModelAdmin):
-    form = PollAdminForm
-    list_display = ('__unicode__', 'start_date', 'end_date', 'admin_get_question_count', 'get_vote_count', )
+    # form = PollAdminForm
+    list_display = ('__unicode__', 'start_date', 'end_date', 'admin_get_question_count', 'get_vote_count',)
     date_hierarchy = "end_date"
-    inlines = [QuestionInline,]
-    
+    inlines = [QuestionInline, ]
+
+
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('__unicode__', 'pool', 'admin_get_votes', 'admin_get_percent', )
+    list_display = ('__unicode__', 'pool', 'admin_get_votes', 'admin_get_percent',)
     list_filter = ('pool',)
-        
+
+
 admin.site.register(Poll, PollAdmin)
 admin.site.register(Question, QuestionAdmin)
