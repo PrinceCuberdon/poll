03af661b2ca5edd64656259bdcc286318d1ad9ae
princecuberdon <LesGensBons64>
2016-06-28 11:58:40 +0200

    New django migration system (yeah... I know)

---
+++ migrations/0001_initial.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Poll',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('question', models.CharField(max_length=150)),
+                ('start_date', models.DateField()),
+                ('end_date', models.DateField()),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Question',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('label', models.CharField(max_length=100)),
+                ('vote', models.IntegerField(default=0)),
+                ('pool', models.ForeignKey(related_name='poll', to='poll.Poll')),
+            ],
+        ),
+    ]
