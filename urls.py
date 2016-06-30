6dce408b1ee0eba6b846781bb927aee815f069b9
princecuberdon <LesGensBons64>
2016-06-30 12:04:19 +0200

    Remove all Python warnings when starting with `python -Wall` option

--- urls.py
+++ urls.py
@@ -2,10 +2,12 @@
 # poll is part of Band Cochon
 # Band Cochon (c) Prince Cuberdon 2011 and Later <princecuberdon@bandcochon.fr>
 #
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
-urlpatterns = patterns('poll',
-    url(r'^vote/$', 'views.vote', name='poll_vote'),
-    url(r'^results/$', 'views.results', name="poll_all"),
-    url(r'^result/(?P<poll_id>\d+)/$', 'views.result', name="poll_result")
-)
+import poll.views
+
+urlpatterns = [
+    url(r'^vote/$', poll.views.vote, name='poll_vote'),
+    url(r'^results/$', poll.views.results, name="poll_all"),
+    url(r'^result/(?P<poll_id>\d+)/$', poll.views.result, name="poll_result")
+]
